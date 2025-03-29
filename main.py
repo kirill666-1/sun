8d2ae11708189825c6daf1887a7d7b961859f8c3
kirill666-1 <kirill.shindyaikin@gmail.com>
2025-03-30 00:48:14 +0300

    Number 1

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+# Инициализируем пустую матрицу для хранения результатов
+    result_matrix = []
+    for i in range(result_rows):
+        # Создаем строку с нулевыми значениями
+        row = [0] * result_cols
+        # Добавляем строку в результирующую матрицу
+        result_matrix.append(row)
 
+    # Заполняем результирующую матрицу вычисленными значениями
+    for result in results:
+        (i, j), value = result
+        result_matrix[i][j] = value  # Присваиваем значение элементу матрицы
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
+    # Записываем результирующую матрицу в файл
+    with open('result_matrix.txt', 'w') as f:
+        for row in result_matrix:
+            # Преобразуем числа в строки
+            str_numbers = [str(num) for num in row]
+            # Объединяем числа через пробел и добавляем перевод строки
+            line = ' '.join(str_numbers) + '\n'
+            # Записываем строку в файл
+            f.write(line)
 
-
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
-
-print(element((1, 0), matrix1, matrix2))
+# Проверяем, является ли данный скрипт основным (а не импортированным модулем)
+if name == 'main':
+    # Запускаем основную функцию
+    main()
